9cbb8de9a5b1decc962b3edcd4d9630984eee279
cerberus999 <aaronespinoza1997@gmail.com>
2024-07-30 12:18:38 -0400

    Tarea

---
+++ Aula.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Aula{
+
+  private ArrayList<Estudiante> estudiantes = new ArrayList<Estudiante>();
+  
+  public ArrayList accion4(){
+    ArrayList<Estudiante> nuevoArreglo = new ArrayList<Estudiante>();
+    while(estudiantes.size() != 0) {
+      int marcadoParaBorrar = 0;
+      Estudiante e = estudiantes.get(marcadoParaBorrar);
+      for(int i=1; i < estudiantes.size(); i++){
+        Estudiante auxiliar = estudiantes.get(i);
+        if(e.nombre.charAt(0) > auxiliar.nombre.charAt(0)){
+          marcadoParaBorrar = i;
+          e = auxiliar;
+        }
+      }
+      estudiantes.remove(marcadoParaBorrar);
+      nuevoArreglo.add(e);
+    }
+    return nuevoArreglo;
+  }
+
+  public int accion1(char letraMayuscula){
+    int respuesta = 0;
+    for(Estudiante e: estudiantes){
+      if(e.apellido_paterno.charAt(0) == letra){
+        respuesta += 1;
+      }
+    }
+    return respuesta;
+  }
+
+  public String accion2(){
+    Estudiante auxiliar = estudiantes.get(0);
+    for(Estudiante e: estudiantes){
+      if (e.estatura > auxiliar.estatura) {
+        auxiliar = e;
+      }
+    }
+    return auxiliar;
+  }
+}
---
+++ Estudiante.java
@@ -0,0 +1,18 @@
+public public class Estudiante {
+  String nombres, apellido_paterno, apellido_materno;
+  int edad;
+  double estatura;
+
+  public Estudiante(String[] datos, int edad, double estatura){
+    nombres = datos[0].toUpperCase();
+    apellido_paterno = datos[1].toUpperCase();
+    apellido_materno = datos[2].toUpperCase();
+    this.edad = edad;
+    this.estatura = estatura;
+  }
+
+  @Override
+  public String toString(){
+    String res = ("Hola soy " + nombres + " " + apellido_paterno + " " + apellido_materno + " tengo " + edad + " y mido " + estatura);
+  }
+}
